e5fb605dd56573fd5a9be7e7f3416c09e940ac0f
Juan Daniel Flores <juandfloresm@gmail.com>
2025-06-17 20:13:10 +0200

    Game Over -> Estadisticas | Win -> Página principal sin estadísticas

--- src/ui/ts/landing.ts
+++ src/ui/ts/landing.ts
@@ -198,7 +198,7 @@ let hydrateTemplate = async (url, params) => {
 				
 				setTimeout(async () => {
 					if (match) {					
-						const t = await (await fetch(`/api/tournament/${match.round.tournamentId}`)).json();
+						const t = await (await fetch(`/api/tournament/${params[0]}`)).json();
 						localStorage.tournament = JSON.stringify(t);
 						location.hash = '#/landing/stats';
 						setTimeout(() => location.hash = `#/landing/${match.round.tournament.game.name}/${match.round.tournament.id}`, 3000);
@@ -206,22 +206,21 @@ let hydrateTemplate = async (url, params) => {
 					} else {
 						const tid = params[0];
 						const t = tid ? await (await fetch(`/api/tournament/${tid}`)).json() : null;
-						if (t) {
+						if (t && t.totalRounds > 1) {
 							localStorage.tournament = JSON.stringify(t);
 							location.hash = '#/landing/stats';
 							setTimeout(() => {
-								location.hash = `#/`;
+								location.hash = '#/landing/matches';
 								localStorage.tournament = '';
 							}, 3000);
 						} else {
-							location.hash = `#/`;
-							localStorage.tournament = '';
+							location.hash = '#/landing/matches';
 						}
 					}
 				}, 3000);
 
 			} else {
-				setTimeout(() => location.hash = '#/landing/stats', 3000);
+				setTimeout(() => location.hash = '#/landing/matches', 3000);
 			}
 			break;
 		case 'nogame':
